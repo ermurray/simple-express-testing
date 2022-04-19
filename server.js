43ce162a143b84e188b75a4d8dbea660f5272fc7
ermurray <ermurray31@gmail.com>
2022-04-18 23:36:19 -0600

    basic map gen

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('html'))
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
